675856c7b1af71d51b503cdd6531e752afa6f225
Laura Imrie <lauraimrie@gmail.com>
2021-10-09 11:57:51 +0000

    Update dependencies

--- test/schema-gen.test.ts
+++ test/schema-gen.test.ts
@@ -2,8 +2,15 @@ import { mapSchema, writeProto } from '@/generator/schema'
 import { getDMMF } from '@prisma/sdk'
 import { join } from 'path'
 
-test('generate grpc schema', async () => {
-    const dmmf = await getDMMF({ datamodelPath: join(__dirname, './test.prisma') })
+test('generate grpc schema (mongo)', async () => {
+    const dmmf = await getDMMF({ datamodelPath: join(__dirname, './mongo.test.prisma') })
+    const schema = mapSchema(dmmf)
+    const proto = await writeProto(schema)
+    expect(proto).toMatchSnapshot()
+})
+
+test('generate grpc schema (postgresql)', async () => {
+    const dmmf = await getDMMF({ datamodelPath: join(__dirname, './sql.test.prisma') })
     const schema = mapSchema(dmmf)
     const proto = await writeProto(schema)
     expect(proto).toMatchSnapshot()
